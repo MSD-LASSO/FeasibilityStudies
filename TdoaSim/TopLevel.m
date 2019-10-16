8567756a90cd7f26319f91548cb13cd174fe4b4e
zeb6290 <zeb6290@rit.edu>
2019-10-16 15:24:07 -0400

    Added variation in z axis to the ellipse

--- TdoaSim/TopLevel.m
+++ TdoaSim/TopLevel.m
@@ -5,28 +5,17 @@
 
 % Setup
 earthRadius = 6371; %km
-elev_low = 200;
-elev_high = 600;
+elev_low = earthRadius + 200;
+elev_high = earthRadius + 600;
 
-
-% Populate Satellite Points
-% t=-pi:0.01:pi;
-% sat_elev = zeros(1, length(t));
-% sat_elev(1:floor(length(t)/2)) = linspace(earthRadius + elev_low, earthRadius + elev_high, floor(length(t)/2));
-% sat_elev(floor(length(t)/2):length(t)) = linspace(earthRadius + elev_high, earthRadius + elev_low, length(t)-floor(length(t)/2)+1);
-% x=sat_elev.*cos(t);
-% y=sat_elev.*sin(t);
-% z=sat_elev.*sin(t);
-% plot3(x,y,z);
-
-%with polar
-theta = -pi:0.01:pi;
+%Satellite Points
+theta = -pi:0.001:pi;
 rho = zeros(1, length(theta));
-rho(1:floor(length(theta)/2)) = linspace(earthRadius + elev_low, earthRadius + elev_high, floor(length(theta)/2));
-rho(floor(length(theta)/2):length(theta)) = linspace(earthRadius + elev_high, earthRadius + elev_low, length(theta)-floor(length(theta)/2)+1);
-polarZ = linspace(20, 20, length(theta));
+rho(1:floor(length(theta)/2)) = linspace(elev_low, elev_high, floor(length(theta)/2));
+rho(floor(length(theta)/2):length(theta)) = linspace(elev_high, elev_low, length(theta)-floor(length(theta)/2)+1);
+polarZ = 400*cos(theta);
 [x, y, z] = pol2cart(theta, rho, polarZ);
 plot3(x, y, z);
-
+axis([-7000 7000 -7000 7000 -2000 2000]);
 
 % Calculate Time Difference
