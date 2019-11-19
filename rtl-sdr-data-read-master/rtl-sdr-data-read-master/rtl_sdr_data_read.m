a1a18c54dff64b6bafba806fa6afea9784a3fedc
Andrew deVries <ard6268@rit.edu>
2019-11-19 11:30:32 -0500

    Added crosscorreltaion

--- rtl-sdr-data-read-master/rtl-sdr-data-read-master/rtl_sdr_data_read.m
+++ rtl-sdr-data-read-master/rtl-sdr-data-read-master/rtl_sdr_data_read.m
@@ -10,21 +10,25 @@
 clear;
 clc;
 close all;
-
-
-% filename 
+% info = audioinfo('Prism.wav');
+% [y,Fs] = audioread('Prism.wav');
+  
 filename1 = 'example_dvb.dat';
+% % filename1 = 'FalconSat3.wav';
 
-% read data from file
+
 fileID1 = fopen(filename1);
 a = fread(fileID1);
 fclose(fileID1); 
-
+% N = 10000;
 % extract I/Q data
 num_samples1 = length(a)/2; % determine number of samples
+% num_samples = N;
 a1 = a(1:num_samples1*2);
 inphase1 = a1(1:2:end) -128;
+% inphase1 = y(1:num_samples,1);
 quadrature1 = a1(2:2:end) -128;
+% quadrature1 = y(1:num_samples,2);
 
 % plot signal in time domain results
 num_plot_samples = length(inphase1);
@@ -47,3 +51,21 @@ spectrum = fftshift(fliplr(10*log10(abs(P))'));
 surf(F,T, spectrum, 'edgecolor', 'none');
 axis tight;
 view(0,90);
+% 
+% fc = 437*10^6;
+% ts = 1/Fs;
+% t = 0:ts:(N-1)*ts;
+% r = zeros(N,1);
+% 
+% for j = 1 : N
+%     r(j) = inphase1(j)  + i * quadrature1(j);
+% end
+% 
+% plot(t, unwrap(angle(r)))
+% hold on
+% % plot(t, inphase1, 'linestyle', '--')
+% % hold on
+% % plot(t, quadrature1, 'linestyle', '--')
+% xlim([0 0.0001])
+% legend('RF_R_X')
+% 
