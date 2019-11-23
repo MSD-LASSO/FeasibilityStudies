fc84e741922b3debe160823e8be29262070b40d3
Anthony I <awi7573@rit.edu>
2019-11-22 20:38:20 -0500

    Attempt 2 for CNN changes

--- TdoaSim/CNN/CNNtrain.m
+++ TdoaSim/CNN/CNNtrain.m
@@ -10,31 +10,31 @@ end
 
 GTtable=table(name,GT(:,1),GT(:,2));
 
-layers = [
-    imageInputLayer([224 224 3])
-    
-    convolution2dLayer(25,8,'Padding','same')
-    batchNormalizationLayer
-    reluLayer
-    
-    maxPooling2dLayer(2,'Stride',2)
-    
-    convolution2dLayer(25,16,'Padding','same')
-    batchNormalizationLayer
-    reluLayer
-    
-    maxPooling2dLayer(2,'Stride',2)
-    
-    convolution2dLayer(25,32,'Padding','same')
-    batchNormalizationLayer
-    reluLayer
-    
-    fullyConnectedLayer(7)
-    reluLayer
-    fullyConnectedLayer(4)
-    reluLayer
-    fullyConnectedLayer(2)
-    regressionLayer]; %how do I add in additional inputs at Fullyconnected layer?
+% layers = [
+%     imageInputLayer([224 224 3])
+%     
+%     convolution2dLayer(25,8,'Padding','same')
+%     batchNormalizationLayer
+%     reluLayer
+%     
+%     maxPooling2dLayer(2,'Stride',2)
+%     
+%     convolution2dLayer(25,16,'Padding','same')
+%     batchNormalizationLayer
+%     reluLayer
+%     
+%     maxPooling2dLayer(2,'Stride',2)
+%     
+%     convolution2dLayer(25,32,'Padding','same')
+%     batchNormalizationLayer
+%     reluLayer
+%     
+%     fullyConnectedLayer(7)
+%     reluLayer
+%     fullyConnectedLayer(4)
+%     reluLayer
+%     fullyConnectedLayer(2)
+%     regressionLayer]; %how do I add in additional inputs at Fullyconnected layer?
 
 % options = trainingOptions('sgdm', ...
 %     'LearnRateSchedule','piecewise', ...
@@ -49,7 +49,7 @@ Valtable=table(name,GT(:,1),GT(:,2));
 epochs = 20; %number of epochs
 miniBatch = 64; % number of images per minibatch
 lR = 5e-6; % learning rate
-% GPUDevice = 1; % which gpu device?
+GPUDevice = 1; % which gpu device?
 L2Reg = 0; % L2 regularization factor
 options = trainingOptions('sgdm', ...
     'Momentum',0.95,...
@@ -64,9 +64,9 @@ options = trainingOptions('sgdm', ...
     'ValidationFrequency',10, ...
     'Shuffle','every-epoch',...
     'Plots','training-progress',...
-    'CheckpointPath','Epochs');
+    'CheckpointPath','C:/Users/awian/Desktop/MachineIntelligence/Epochs');
 
-%     'ExecutionEnvironment','gpu'
+%     
 % 'GradientDecayFactor',0.9, %default is 0.9
 %    'SquaredGradientDecayFactor' ,0.999,
 %     
@@ -77,8 +77,8 @@ options = trainingOptions('sgdm', ...
 %     'ValidationData',Valtable,...
 %     'Plots','training-progress');
         
-load BasedOnResnet
+load C:/Users/awian/Desktop/MachineIntelligence/Resnet101Modified.mat
 % net = trainNetwork(GTtable,layers,options);
-net=trainNetwork(GTtable,lgraph_1,options); 
+net=trainNetwork(GTtable,lgraph_2,options); 
 
 % ypredict=predict(net,Valtable(:,1));
