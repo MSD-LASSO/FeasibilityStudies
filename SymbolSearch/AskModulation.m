971ec383a9911cfada218372b91b5ad7f9f52f6a
zeb6290 <zeb6290@rit.edu>
2019-11-26 10:05:07 -0500

    Ask modulation written, not working (see plots in AskModulationTest).

--- SymbolSearch/AskModulation.m
+++ SymbolSearch/AskModulation.m
@@ -1,7 +1,34 @@
-function [ binaryData ] = AskModulation( data, fs )
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
+function [ digitalData ] = AskModulation( data )
 
+lengthData = 2000;
+shortData = data(1:lengthData);
+clear data;
+
+% need to filter the signal
+% [peaks, peakIdx] = findpeaks(data);
+% period = mean(diff(peakIdx)); % approximate period
+
+ampMax = max(shortData);
+ampMin = min(shortData);
+
+% 4 amplitude levels
+threshold1 = ampMin + (ampMax - ampMin)*1/4;
+threshold2 = ampMin + (ampMax - ampMin)*2/4;
+threshold3 = ampMin + (ampMax - ampMin)*3/4;
+
+digitalData = zeros(1, length(shortData));
+
+for i = 1:length(shortData)
+    if (shortData(i) < threshold1)
+        digitalData(i) = 0;
+    elseif (shortData(i) < threshold2)
+        digitalData(i) = 1;
+    elseif (shortData(i) < threshold3)
+        digitalData(i) = 2;
+    else
+        digitalData(i) = 3;
+    end
+end
 
 end
 
--- SymbolSearch/AskModulationTest.m
+++ SymbolSearch/AskModulationTest.m
@@ -1,1 +1,62 @@
-[data, Fs] = audioread('Loc1_101300000Hz_IQ_Test5.wav');
+clearvars;
+close all;
+hold off;
+
+dataSize = 2000;
+
+% create province input signal
+[data_pv, fs_pv] = audioread('Loc2_97900000Hz_IQ(Province).wav'); % sample frequency and data
+x_pv = data_pv(1:dataSize); % shorten data
+clear data_pv;
+
+% plot input signal
+figure();
+t_rep = 1/fs_pv; % period of "continuous" input signal
+t_low = 0;
+t_high = t_rep*(length(x_pv) - 1);
+t_pv = t_low:t_rep:t_high; % define t interval
+
+plot(t_pv, x_pv);
+title('Input Signal - Province');
+xlabel('t [s]');
+
+% Ask Modulation
+digitalData_pv = AskModulation(x_pv);
+
+% Plot digital data
+figure();
+sample = 1:length(digitalData_pv);
+
+plot(sample, digitalData_pv, '.');
+title('Digital Signal - Province');
+ylim([0.5, 3.5]);
+xlabel('sample');
+
+% read mission control input signal
+[data_ms, fs_ms] = audioread('Loc1_97900000Hz_IQ(Mission Control).wav'); % sample requency and data
+x_ms = data_ms(1:dataSize); % shorten data
+clear data_ms;
+
+% plot input signal
+figure();
+t_rep = 1/fs_ms; % period of "continuous" input signal
+t_low = 0;
+t_high = t_rep*(length(x_ms) - 1);
+t_ms = t_low:t_rep:t_high; % define t interval
+
+plot(t_ms, x_ms);
+title('Input Signal - Mission Control');
+xlabel('t [s]');
+
+% Ask Modulation
+digitalData_ms = AskModulation(x_ms);
+
+% Plot digital data
+figure();
+sample = 1:length(digitalData_ms);
+
+plot(sample, digitalData_ms, '.');
+title('Digital Signal - Mission Control');
+ylim([0.5, 3.5]);
+xlabel('sample');
+
