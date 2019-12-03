5c7bd8dc3a957ac44cdecd1b72c58a7975e52558
Anthony I <awi7573@rit.edu>
2019-12-02 19:48:56 -0500

    Added a range approximation model and updated sensitivity data creation

--- TdoaSim/CNN/getRandSat.m
+++ TdoaSim/CNN/getRandSat.m
@@ -1,9 +1,10 @@
-function [az,el,rng] = getRandSat(AzimuthRange,ElevationRange,SatelliteRangeRange)
+function [az,el,rng] = getRandSat(AzimuthRange,ElevationRange,SatelliteRangeRange,P)
 %Creates a random set of coordinates based on the input ranges. Assumes a
 %uniform distribution. Input ranges are of form [low high].
 az=rand(1)*diff(AzimuthRange)+AzimuthRange(1);
 el=rand(1)*diff(ElevationRange)+ElevationRange(1);
-rng=rand(1)*diff(SatelliteRangeRange)+SatelliteRangeRange(1);
+al=rand(1)*diff(SatelliteRangeRange)+SatelliteRangeRange(1);
+rng=RangeApproximate(el,al,P);
 
 end
 
