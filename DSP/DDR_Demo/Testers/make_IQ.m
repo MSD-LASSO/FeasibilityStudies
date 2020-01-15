863bbf962119ad63db9b739576ec810da067a470
Andrew deVries <ard6268@rit.edu>
2020-01-15 16:18:04 -0500

    Updated IQ file generation code

--- DSP/DDR_Demo/Testers/make_IQ.m
+++ DSP/DDR_Demo/Testers/make_IQ.m
@@ -7,8 +7,8 @@ x1 = zeros(N, 1);
 m1 = zeros(N, 1);
 for k = 1:N
     t(k) = Ts*(k-1);
-    m1(k) = sin(2*pi()*t(k));
-    x1(k) = cos(2*pi()*BW/2*t(k) + BW/2*sin(m1(k)));% + 1i * sin(2*pi()*BW*t(k) + sin(m1(k)));
+    m1(k) = sin(BW/2*t(k)) + 0.3*sin(BW/6*t(k));
+    x1(k) = cos(m1(k)) + 1i * sin(m1(k));
 end
 x2(:, 1) = real(x1);%/sqrt(2);
 x2(:, 2) = imag(x1);%/sqrt(2);
