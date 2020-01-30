2a82e081127d0c83d01a6a4aaaeb09f6f9e543a1
Anthony I <awi7573@rit.edu>
2020-01-30 10:36:04 -0500

    Added test harness. Still need to populate tests

---
+++ OrekitAccessDopplerCalculator/test/AdditionalTests.java
@@ -0,0 +1,31 @@
+import org.junit.Test;
+
+public class AdditionalTests {
+
+
+
+    @Test
+    public void ValueRangeTest(){
+
+    }
+
+    @Test
+    public void findsAllEvents(){
+
+    }
+
+    @Test
+    public void startsInMiddleOfEvent(){
+
+    }
+
+    @Test
+    public void endsInMiddleOfEvent(){
+
+    }
+
+    @Test
+    public void ApproximateDopplerComparison(){
+
+    }
+}
---
+++ OrekitAccessDopplerCalculator/test/CelestrakImporterTest.java
@@ -0,0 +1,22 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CelestrakImporterTest {
+
+
+    @Test
+    public void getTLEtest(){
+
+    }
+
+    @Test
+    public void failToFindTest(){
+
+    }
+
+    @Test
+    public void notConnectedToInternetTest(){
+
+    }
+
+}
