545ac315a64ac881b8366629abdf0f7889d991c5
Anthony I <awi7573@rit.edu>
2020-02-10 20:00:41 -0500

    Added TCP

---
+++ TdoaSim/TCPin.m
@@ -0,0 +1,17 @@
+host='127.0.0.1';
+port=5010;
+t = tcpclient(host, port)
+
+%Need to figure out my own IP address
+%is the firewall blocking anything?
+%Figure out the inputs we are getting
+%Are those inputs the same every time?
+
+%24 inputs?
+%we will send them all in a single byte stream.
+%we will parse that stream in some manner
+
+
+%for the server,
+%need to install matlab if I want to work in there.
+%work in the root. 
