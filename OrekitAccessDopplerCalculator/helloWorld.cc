2dfb48e2db4cb7dc6ccf487224bdcfd50a0485be
Matthew Truong <mtruong@Matthews-MacBook-Pro-4.local>
2020-03-24 13:24:24 -0400

    Updated files to add code to change elevations. Need to add way to change between printing frequencies or elevations easily

---
+++ OrekitAccessDopplerCalculator/helloWorld.cc
@@ -0,0 +1,14 @@
+// basic file operations
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main () {
+ // ofstream myfile;
+ // myfile.open ("example.txt");
+ // myfile << "Writing this to a file.\n";
+ // myfile.close();
+ cout << "Hello World \n"; // prints Hello World
+ 
+ return 0;
+
+}
